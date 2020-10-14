b7746099324f9494233f3a10ea87b6689da94ed0
Harmony Kunzler <hkunzler@gmail.com>
2020-10-14 17:25:35 -0400

    fix media links

--- src/components/header/nav/Nav.js
+++ src/components/header/nav/Nav.js
@@ -21,7 +21,7 @@ import navStyles from "./navStyles.js";
 export default function Nav() {
   const classes = navStyles();
   const navItems = ["Home", "Bio", "Videos", "News", "Articles", "Products"];
-  const socialMedia = {
+  const socialMediaLinks = {
     facebook: "https://www.facebook.com/Kokayi137",
     twitter: "https://twitter.com/kokayi137",
     linkedIn: "https://www.linkedin.com/in/kokayi-nosakhere-8a5a877/",
