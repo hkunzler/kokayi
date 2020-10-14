72e9e85670241099d6d3fea9278e00881058b37d
Harmony Kunzler <hkunzler@gmail.com>
2020-10-14 17:52:49 -0400

    temp remove icons

--- src/components/header/nav/Nav.js
+++ src/components/header/nav/Nav.js
@@ -21,7 +21,7 @@ import navStyles from "./navStyles.js";
 export default function Nav() {
   const classes = navStyles();
   const navItems = ["Home", "Bio", "Videos", "News", "Articles", "Products"];
-  const socialMediaLinks = {
+  const socialMedia = {
     facebook: "https://www.facebook.com/Kokayi137",
     twitter: "https://twitter.com/kokayi137",
     linkedIn: "https://www.linkedin.com/in/kokayi-nosakhere-8a5a877/",
@@ -46,7 +46,7 @@ export default function Nav() {
                 <Link className={classes.socialMediaLinks} to="/home">
                   <h2>Kokayi Nosakhere</h2>
                 </Link>
-                <div className="icons">{socialMediaLinks}</div>
+                {/* <div className="icons">{socialMediaLinks}</div> */}
               </div>
               <Hidden smDown>
                 <List
